58291a7258f0de3a4826023ebfbb10bbae5a4475
kawa1228 <sxa.1105bo@gmail.com>
2020-03-29 05:48:03 +0900

    fix fetch all records

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const puppeteer = require('puppeteer');
 const config = require('./config.js');
+const csv = require('./csv.js');
 
 async function main() {
-    console.log('-----main----')
+    console.log('main ...')
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     try {
-        await fetchData(page);
+        const records = await fetchRecords(page);
+        csv.create(records);
     } catch(error) {
         console.log('エラー：', error);
     } finally {
@@ -16,28 +18,37 @@ async function main() {
     }
 }
 
-async function fetchData(page) {
-    console.log('-----fetchData----')
+// puppeteer経由でアクセス
+async function fetchRecords(page) {
+    console.log('fetchRecords ...')
     await page.goto(config.TARGET_URL);
 
-    const text = await page.evaluate(() => {
-        const composer = {};
-        const card = document.querySelector('dl');
-        const name = card.querySelector('dt > a > b').innerText;
-        const nameSplit = name.split(/（|）/gi);
-        composer['name_jp'] = nameSplit[1];
-        composer['name_origin'] = nameSplit[0];
-        composer['periods'] = card.querySelector('dt > a > span').innerText;
-        composer['description'] = card.querySelector('dd').innerText;
+    const records = await page.evaluate(() => {
+        const node = document.querySelectorAll('dl');
+        const list = Array.from(node);
 
-        const texts = card.querySelector('dt > a').innerText
-        const detail = texts.split(/\n/gi);
-        composer['home_city'] = detail[1];
-        composer['activity'] = detail[2];
+        // 必要なデータを配列に格納
+        const data = list.map((item) => {
+            const obj = {};
 
-        return composer;
+            const name = item.querySelector('dt > a > b').innerText;
+            const nameSplit = name.split(/（|）/gi);
+            obj['name_jp'] = nameSplit[1];
+            obj['name_origin'] = nameSplit[0];
+
+            obj['periods'] = item.querySelector('dt > a > span').innerText;
+            obj['description'] = item.querySelector('dd').innerText;
+
+            const linkText = item.querySelector('dt > a').innerText
+            const linkTextSplit = linkText.split(/\n/gi);
+            obj['home_city'] = linkTextSplit[1];
+            obj['activity'] = linkTextSplit[2];
+
+            return obj;
+        });
+        return data;
     });
-    console.log(text);
+    return records;
 }
 
 main();
